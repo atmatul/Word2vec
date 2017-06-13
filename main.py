bedba094f4f55bbf870b7c1f745200908d7e1510
Kunal Pal <mail.kunalpal@gmail.com>
2017-06-14 01:19:39 +0200

    Training on shiva trilogy from Amish Tripathi

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-import pandas as pd
+import os
 import nltk
 import gensim
+import helper.clean as cleaner
+from tqdm import tqdm
 
 # Prepare data for training
-df = pd.read_csv('corpus/random/jokes.csv');
-x = df['Question'].values.tolist()
-y = df['Answer'].values.tolist()
-corpus = x + y
+_relative_path = 'corpus/shiva/'
+corpus = []
+for filename in os.listdir(os.path.join(os.getcwd(), _relative_path)):
+    _filepath = os.path.join(_relative_path, filename)
+    # df = pd.read_csv(_filepath, sep=None)
+    # corpus = corpus + df['Raw_text']
+    with open(_filepath) as file:
+        corpus = corpus + file.read().split('\n')
 
-# Tokenize data for word2vec creation
-tok_corp = [nltk.word_tokenize(sent) for sent in corpus]
+# Tokenize and clean data for word2vec creation
+print('\nTokenizing words:')
+tokenized_corpus = [nltk.word_tokenize(sent) for sent in tqdm(corpus)]
+tokenized_corpus_no_punc = cleaner.clean_punctuations(tokenized_corpus)
+tokenized_corpus_no_stop = cleaner.clean_stopwords(tokenized_corpus_no_punc)
 
 # Creating vectors from the words
-model = gensim.models.Word2Vec(tok_corp, min_count=1, size=32)
+model = gensim.models.Word2Vec(tokenized_corpus, min_count=3, size=32)
 
 # Saving and retrieving the trained model (for sanity checks)
-# model.save('saved/model-jokes')
-trained_model = gensim.models.Word2Vec.load('saved/model-jokes')
+model.save('saved/model-shiva')
+
+# -- Comment before here to use pretrained-data
+trained_model = gensim.models.Word2Vec.load('saved/model-shiva')
 
 # Testing the model
-print(trained_model.wv.most_similar('boy'))
+print(trained_model.wv.most_similar('Shiva'))
